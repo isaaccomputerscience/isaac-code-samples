cd48572b7ebf2af043a3616ef53c6bcf2cbdfa5a
m-bowley <mac@raspberrypi.org>
2021-05-06 13:46:58 +0100

    Adding in the right amount of loops

--- gcse-programming-concepts/programming-constructs/for_loop.cs
+++ gcse-programming-concepts/programming-constructs/for_loop.cs
@@ -6,7 +6,7 @@ namespace isaac_code_samples
     {
         void Main(string[] args){
             /// For loop that prints a line five times
-            for(int i = 0; i < 5; i++){
+            for(int i = 0; i < 2; i++){
                 Console.WriteLine("Hip, Hip, Hooray!");
             }
 
