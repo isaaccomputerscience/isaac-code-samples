378292ffa189fd516137917005955311f7e4caea
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-05-11 15:00:58 +0100

    updated test script

--- searching_algorithms/binary_search/binary_search_recursive.py
+++ searching_algorithms/binary_search/binary_search_recursive.py
@@ -15,31 +15,25 @@ def binary_search (items, search_item, first, last):
 
 def test():
     '''test script for binary search'''
-    # Create a list containing numbers 0 to 7
-    items = [] 
-    for i in range(8):
-        items.append(i)
+    # Create a list containing some names
+    items = ['Albie', 'Byron', 'Divya', 'Hari', 'Joanne', 'Kim', 'Wilbur', 'Zoe'] 
+    first = 0
+    last = 7
+    
     # Search for first item in the list
-    print ('Searching for first item in the list')
-    result = binary_search(items, 0, 0, 7)
-    if result != 0:
-        print ('test failed')
-    else:
-        print ('test passed')
+    print ('Searching for first name in the list')
+    result = binary_search(items, 'Albie', first, last)
+    print ('Result of search was {}'.format(result))
+    
     # Search for last item in the list
-        print ('Searching for last item in the list')
-    result = binary_search(items, 7, 0, 7)
-    if result != 7:
-        print ('test failed')
-    else:
-        print ('test passed')
-    # Search an item that is not in the list
-    print ('Searching for item that is not in the list')
-    result = binary_search(items, 12, 0, 7)
-    if result != -1:
-        print ('test failed')
-    else:
-        print ('test passed')
+    print ('Searching for the last name in the list')
+    result = binary_search(items, 'Zoe', first, last)
+    print ('Result of search was {}'.format(result))
+    
+    # Search for an item that is not in the list
+    print ('Searching for a name that is not in the list')
+    result = binary_search(items, 'Boris', first, last)
+    print ('Result of search was {}'.format(result))
 
 if __name__ == '__main__':
     test()
