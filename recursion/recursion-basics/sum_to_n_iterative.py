1aa4ca5aaf5ca0d8c851799aced97b9ee8cb6e78
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-05-24 11:05:11 +0100

    Create sum_to_n_iterative.py
    Initial version

---
+++ recursion/recursion-basics/sum_to_n_iterative.py
@@ -0,0 +1,12 @@
+def sum_to_n_iterative(n):
+    '''returns the sum of all natural numbers from 1 to n inclusive'''
+    total = 0
+    for i in range (1, n):
+        total = total + i
+    return total
+
+    
+if __name__ =='__main__':
+    n = 6
+    result = sum_to_n_iterative(n)
+    print ('The sum of 1 to {} is: {}'.format(n, result))
