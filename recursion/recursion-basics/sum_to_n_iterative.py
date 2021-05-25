e52b2503206d096f97ed86f7e41fc50ee70f193d
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-05-25 15:52:40 +0100

    Update sum_to_n_iterative.py
    corrected small error

--- recursion/recursion-basics/sum_to_n_iterative.py
+++ recursion/recursion-basics/sum_to_n_iterative.py
@@ -1,7 +1,7 @@
 def sum_to_n_iterative(n):
     '''returns the sum of all natural numbers from 1 to n inclusive'''
     total = 0
-    for i in range (1, n):
+    for i in range (1, n + 1):
         total = total + i
     return total
 
