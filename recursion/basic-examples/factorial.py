dd1f2b3c2807f3895caded281f6dbf500927eac9
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-05-25 10:19:33 +0100

    initial version

---
+++ recursion/basic-examples/factorial.py
@@ -0,0 +1,10 @@
+def factorial (n):
+    ''' Returns the value of n!'''
+    if n == 1:
+        return 1
+    else:
+        return  n * factorial (n-1)
+
+
+if __name__ == '__main__':
+    print (factorial (5))
