c16f3570d06878dad578fb6d39eec3a8c7919f0b
m-bowley <mac@raspberrypi.org>
2021-05-28 09:58:40 +0100

    Adding in palindrome

---
+++ recursion/recursion-examples/palindrome.cs
@@ -0,0 +1,37 @@
+/*
+Note: This file is designed to be copied out and compiled on your machine. 
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Compile the program
+4. Run the program
+*/
+
+using System;
+
+namespace isaac_code_samples
+{
+    class PalindromeRecursive
+    {
+        static void Main(string[] args){
+            string test_word = "kayak";
+            bool is_palindrome = Palindrome(test_word);
+            Console.WriteLine(String.Format("{0}: {1}", test_word, is_palindrome.ToString()));
+        }
+
+        public static bool Palindrome(string phrase){
+            //Returns True if string is a palindrome
+            int length = phrase.Length;
+            if (length == 0 || length == 1){
+                return true;
+            }else if(phrase[0] == phrase[length - 1]){
+                string new_phrase = phrase.Substring(1, length - 2);
+                return Palindrome(new_phrase);
+            }else {
+                return false;
+            }
+        }
+    }
+}
