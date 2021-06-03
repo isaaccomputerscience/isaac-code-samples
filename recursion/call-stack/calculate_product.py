8727383b0b753212f8a052dc39957533c4861fe8
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-06-03 15:21:40 +0100

    corrected docstrings

--- recursion/call-stack/calculate_product.py
+++ recursion/call-stack/calculate_product.py
@@ -1,17 +1,17 @@
 def calculate_product(n1, n2):
-    ''' Calculates the product of two numbers '''
+    """Calculates the product of two numbers."""
     product = n1 * n2
-    print (product)
+    print(product)
 
 
 def main():
-    ''' Simple program to demonstrate use of call stack '''
-    num1 = input ('Enter a number ')
-    num2 = input ('Enter another number ')
-    num1_int=int(num1)
-    num2_int=int(num2)
+    """Simple program to demonstrate use of call stack."""
+    num1 = input("Enter a number ")
+    num2 = input("Enter another number ")
+    num1_int = int(num1)
+    num2_int = int(num2)
     calculate_product(num1_int, num2_int)
-    print ('I hope you enjoyed using the product calculator')
+    print("I hope you enjoyed using the product calculator")
 
 
 if __name__ == '__main__':
