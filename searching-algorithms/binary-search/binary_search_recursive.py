49717b37e15f058a086e3d391e9bf660686c3db2
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-06-03 15:52:34 +0100

    Updates quotes and docstring

--- searching-algorithms/binary-search/binary_search_recursive.py
+++ searching-algorithms/binary-search/binary_search_recursive.py
@@ -1,5 +1,5 @@
 def binary_search (items, search_item, first, last):
-    '''Binary search - recursive version'''
+    """Binary search - recursive version."""
     if first > last:
         return -1
     else:
@@ -8,31 +8,31 @@ def binary_search (items, search_item, first, last):
             return midpoint
         elif search_item > items[midpoint]:
             first = midpoint + 1
-            return binary_search (items, search_item, first, last)
+            return binary_search(items, search_item, first, last)
         else:
             last = midpoint - 1 
-            return binary_search (items, search_item, first, last)
+            return binary_search(items, search_item, first, last)
 
 def test():
-    '''test script for binary search'''
+    """Test script for binary search."""
     # Create a list containing some names
-    items = ['Albie', 'Byron', 'Divya', 'Hari', 'Joanne', 'Kim', 'Wilbur', 'Zoe'] 
+    items = ["Albie", "Byron", "Divya", "Hari", "Joanne", "Kim", "Wilbur", "Zoe"] 
     first = 0
     last = 7
     
     # Search for first item in the list
-    print ('Searching for first name in the list')
-    result = binary_search(items, 'Albie', first, last)
+    print ("Searching for first name in the list")
+    result = binary_search(items, "Albie", first, last)
     print ('Result of search was {}'.format(result))
     
     # Search for last item in the list
     print ('Searching for the last name in the list')
-    result = binary_search(items, 'Zoe', first, last)
+    result = binary_search(items, "Zoe", first, last)
     print ('Result of search was {}'.format(result))
     
     # Search for an item that is not in the list
     print ('Searching for a name that is not in the list')
-    result = binary_search(items, 'Boris', first, last)
+    result = binary_search(items, "Boris", first, last)
     print ('Result of search was {}'.format(result))
 
 if __name__ == '__main__':
