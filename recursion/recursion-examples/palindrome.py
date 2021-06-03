61136f2bb39cb36f2f3075d6da29b2e67c749a8d
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-06-03 15:34:10 +0100

    Updated docstring

--- recursion/recursion-examples/palindrome.py
+++ recursion/recursion-examples/palindrome.py
@@ -1,17 +1,16 @@
-def palindrome(string):
-    ''' Returns True if string is a palindrome '''
-    print (string)
-    length = len (string)
+def palindrome(word):
+    """Returns True if word is a palindrome"""
+    length = len(word)
     if length == 0 or length == 1:
         return True
-    elif string[0] == string[length - 1]:
-        new_string = string[1 : length - 1]
-        return palindrome (new_string)
+    elif word[0] == word[length - 1]:
+        new_word = word[1 : length - 1]
+        return palindrome (new_word)
     else:
         return False
 
 
 if __name__ == '__main__':
     test_word = 'kayak'
-    is_palindrome = palindrome (test_word)
-    print ('{}: {}'.format(test_word, is_palindrome))
+    is_palindrome = palindrome(test_word)
+    print ("{}: {}".format(test_word, is_palindrome))
