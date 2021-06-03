fe44058bf6ab0657ba6fdf92721bb1669d7f477f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-06-03 15:30:33 +0100

    Updated docstring

--- recursion/recursion-examples/gcd_recursive.py
+++ recursion/recursion-examples/gcd_recursive.py
@@ -1,13 +1,13 @@
 def gcd(x, y):
-    ''' Euclidian algorithm to find greatest common denominator '''
+    """Euclidian algorithm to find greatest common denominator."""
     if y == 0:
         return x
     else:
-        return gcd (y, x % y)
+        return gcd(y, x%y)
     
 
 if __name__ == '__main__':
     x = 259
     y = 111
-    answer = gcd (x, y)
-    print ('The lowest common denominator of {} and {} is {}'.format(x, y, answer))
+    answer = gcd(x, y)
+    print("The lowest common denominator of {} and {} is {}".format(x, y, answer))
