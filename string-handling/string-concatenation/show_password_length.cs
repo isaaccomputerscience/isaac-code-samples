ae3f3529c1cc25470c60d389b2bcebd812076aef
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-07 13:33:11 +0100

    Create show_password_length.cs

---
+++ string-handling/string-concatenation/show_password_length.cs
@@ -0,0 +1,30 @@
+/*
+Note: This file is designed to be copied out and compiled on your machine. 
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Compile the program
+4. Run the program
+*/
+
+using System;
+
+namespace isaac_code_samples
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            ShowPasswordLength();
+         }
+
+        // Returns the sum of all natural numbers from 1 to n inclusive
+        public static void ShowPasswordLength()
+        {
+            Console.Write("Please enter a password ");
+            string password = Console.ReadLine();
+            Console.WriteLine(password.Length);
+        }
+    }
+}
