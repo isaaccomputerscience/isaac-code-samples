d08b514553a4211c449fa025d289db28318eae3b
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-07 13:16:37 +0100

    Create greetings.cs

---
+++ string-handling/string-concatenation/greetings.cs
@@ -0,0 +1,38 @@
+/*
+Note: This file is designed to be copied out and compiled on your machine. 
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Compile the program
+4. Run the program
+*/
+
+using System;
+
+namespace isaac_code_samples
+{
+    class StringConcatenation
+    {
+        static void Main(string[] args)
+        {
+
+            Greetings();           
+
+        }
+
+        // Returns the sum of all natural numbers from 1 to n inclusive
+        public static void Greetings()
+        {
+
+            Console.Write("Please enter your first name ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Greetings " + firstName);
+            Console.Write("Please enter your surname ");
+            string surname = Console.ReadLine();
+            string userName = firstName + " " + surname;
+            Console.WriteLine("Greetings " + userName);
+
+        }
+    }
+}
