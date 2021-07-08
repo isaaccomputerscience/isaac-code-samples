1c6e2564bbca48698ea45f4a5f2d017017e9f356
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-08 15:45:49 +0100

    used f string

--- gcse-programming-concepts/casting/string_to_float.py
+++ gcse-programming-concepts/casting/string_to_float.py
@@ -1,10 +1,10 @@
 def double_it():
-    """prompts user for a number and doubles it"""
+    """Prompts user for a number and doubles it"""
     number = input("Please enter a number ")
     float_number = float(number)
     answer = float_number * 2
-    print('The answer is ',answer)
-    print('{:.2f}'.format(answer))
+    print(f"The answer is {answer}")
+    print(f"{answer:.2f}")
 
 if __name__ == '__main__':
     double_it()
