186e28b4d887d62e05a117b484bc5941194a1947
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-08 15:53:50 +0100

    adjusted spacing

--- subroutines/call-stack/calculate_pay.py
+++ subroutines/call-stack/calculate_pay.py
@@ -1,17 +1,22 @@
+# Isaac Computer Science
+# Iterative version of binary search
+# Usage licensed under the Open Government Licence v3.0
+
+
 def calculate_pay(h, r):
-    """Performs a basic pay calculation."""
+    """Performs a basic pay calculation"""
     pay =  h * r
     return pay
 
 
 def main():
-    """A simple pay calculation program."""
+    """A simple pay calculator"""
     hours = input('Enter hours worked ')
     rate = input('Enter hourly rate ')
     hours_int = float(hours)
     rate_int = float(rate)
     pay = calculate_pay(hours_int, rate_int)
-    print('Your pay is £{:.2f}'.format(pay))
+    print(f"Your pay is £{pay:.2f}")
 
 
 if __name__ == '__main__':
