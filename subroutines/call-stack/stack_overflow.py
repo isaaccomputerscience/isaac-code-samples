a8dbf58ad55d32020ab7305f2b13e068e64c1e84
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-08 15:57:41 +0100

    added header

--- subroutines/call-stack/stack_overflow.py
+++ subroutines/call-stack/stack_overflow.py
@@ -1,5 +1,9 @@
+# Isaac Computer Science
+# Iterative version of binary search
+# Usage licensed under the Open Government Licence v3.0
+
 def stack_overflow():
-    ""Simple program to force stack overflow."""
+    """Simple program to force stack overflow"""
     stack_overflow()
 
     
