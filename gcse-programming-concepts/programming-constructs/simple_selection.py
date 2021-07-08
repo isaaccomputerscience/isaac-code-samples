48d6fc5664321aa9957650641bc67603b310eba4
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-08 15:48:38 +0100

    adjusted spacing

--- gcse-programming-concepts/programming-constructs/simple_selection.py
+++ gcse-programming-concepts/programming-constructs/simple_selection.py
@@ -1,10 +1,10 @@
 def guess_the_animal():
-    """An example of a simple selection statement"""
-    animal = input ("Which animal has a long bendy trunk? ")
+    """Example of a simple selection statement"""
+    animal = input("Which animal has a long bendy trunk? ")
     if animal == "elephant":
-        print ("You are correct")
+        print("You are correct")
     else:
-        print ("You are incorrect")
+        print("You are incorrect")
 
 if __name__ == '__main__':
     guess_the_animal()
