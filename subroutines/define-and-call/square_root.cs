f8d7d1fa7435df9fb1b95e136380c5c55fcd1e5a
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-15 15:42:13 +0100

    Create square_root.cs

---
+++ subroutines/define-and-call/square_root.cs
@@ -0,0 +1,28 @@
+/*
+Note: This file is designed to be copied out and compiled on your machine. 
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Compile the program
+4. Run the program
+*/
+
+using System;
+
+
+namespace isaac_code_samples
+{
+    class subroutines
+    {
+        // The Main method is the entry point for all C# programs
+        static void Main(string[] args)
+        {
+
+            double SquareRoot = Math.Sqrt(9);
+            Console.WriteLine(SquareRoot);
+
+        }
+
+    }
+}
