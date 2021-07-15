c0f38b8b9230dc0b277ff94489deaf946423efe8
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-15 15:30:17 +0100

    Create flow_of_control.cs

---
+++ subroutines/define-and-call/flow_of_control.cs
@@ -0,0 +1,33 @@
+/*
+Note: This file is designed to be copied out and compiled on your machine. 
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Compile the program
+4. Run the program
+*/
+
+using System;
+
+namespace isaac_code_samples
+{
+    class subroutines
+    {
+        // The Main method is the entry point for all C# programs
+        static void Main(string[] args)
+        {
+
+            // Extra code is included to make sample execute
+            int minutes = 90;
+
+            // Sample code
+            int seconds = minutes * 60;
+            int milliseconds = seconds * 1000;
+
+            Console.WriteLine($"Seconds: {seconds}, Milliseconds: {milliseconds}");
+
+        }
+
+    }
+}
