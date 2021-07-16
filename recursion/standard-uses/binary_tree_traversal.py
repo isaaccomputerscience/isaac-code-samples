b83cc6d0a261a14133a84d7ee22785fbc73cf865
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-16 16:31:06 +0100

    Standards update

--- recursion/standard-uses/binary_tree_traversal.py
+++ recursion/standard-uses/binary_tree_traversal.py
@@ -1,15 +1,18 @@
-from BST_classes import BST, Node # Uses a binary search tree
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+from BST_classes import BST, Node  # Uses a binary search tree
 
 def traverse_in_order(node):
-    """In-order binary tree traversal."""
+    """In-order binary tree traversal"""
     if node != None:
         traverse_in_order(node.left)
         print(node.data)
         traverse_in_order(node.right)
 
 
-def test_data(bst):
-    ''' Inserts some test data into tree '''
+def insert_test_data(bst):
+    """Inserts some test data into tree"""
     bst.insert('Zac')
     bst.insert('Lucy')
     bst.insert('Anil')
@@ -18,6 +21,6 @@ def test_data(bst):
        
 if __name__ == '__main__':
     my_tree = BST()
-    test_data(my_tree)  # Create some test data
+    insert_test_data(my_tree)  # Create some test data
     start = my_tree.get_root()  # Get start node
     traverse_in_order(start) 
