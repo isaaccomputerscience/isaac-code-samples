511955a373071609c9c939f8f3cc13a731c06d6f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-16 16:29:14 +0100

    Standards update

--- recursion/standard-uses/BST_classes.py
+++ recursion/standard-uses/BST_classes.py
@@ -1,8 +1,11 @@
-# in this example, a class is defined for a Node. However, it used as a Record (i.e. no methods are defined)
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+# In this example, a class is defined for a Node. However, it used here as a record structure (i.e. no methods are defined)
 
 
 class Node:
-    """Node for a binary search tree."""
+    """Node for a binary search tree"""
     def __init__(self, data):
         self.data = data
         self.left = None
@@ -13,15 +16,17 @@ class BST:
     """Binary search tree."""
     def __init__(self):
         self.root = None
+        
 
     def get_root(self):
         """Returns value of root node."""
         return self.root
+    
 
     def insert (self, data):
         """Insert new node."""
         new_node = Node(data)
-        if self.root == None: # new root node
+        if self.root == None:  # New root node
             self.root = new_node
         else:                   
             placed = False
