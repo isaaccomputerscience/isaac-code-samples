55a29bb29776d64cfbdaa8c137677bdeb4c9c226
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-16 15:45:46 +0100

    Standards update

--- recursion/recursion-examples/factorial.py
+++ recursion/recursion-examples/factorial.py
@@ -1,4 +1,7 @@
-def factorial (n):
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def factorial(n):
     """Recursively calculates and returns the value of n!"""
     if n == 1:
         return 1
@@ -8,5 +11,5 @@ def factorial (n):
 
 if __name__ == '__main__':
     n = 5
-    result = factorial (n)
+    result = factorial(n)
     print(f"{n}! is {result}")
