e949ded37a22c273c056f5c4035b0b2f85ef2c22
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-16 15:15:21 +0100

    Standaards update

--- recursion/recursion-basics/sum_to_n.py
+++ recursion/recursion-basics/sum_to_n.py
@@ -1,9 +1,15 @@
+# Isaac Computer Science
+# Iterative version of binary search
+# Usage licensed under the Open Government Licence v3.0
+
+
 def sum_to_n(n):
-    """Recursively calculates and returns the sum of all natural numbers from 1 to n inclusive."""
+    """Recursively calculates and returns the sum of all natural numbers from 1 to n inclusive"""
     if n == 1:
         return 1
     else:
         return n + sum_to_n(n-1)
+
     
 if __name__ =='__main__':
     n = 6
