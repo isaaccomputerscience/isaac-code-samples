ee31e054582407da051e5645585ed8d3337c9cc7
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 16:56:23 +0100

    Create change_string.py

---
+++ string-handling/string-characteristics/change_string.py
@@ -0,0 +1,23 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def change_string(my_string):
+    """Changes all lowercase a to lowercase b"""
+    new_string = ""  # New empty string
+    for char in my_string:
+        if char == "a":
+            new_string = new_string + "b"
+        else:
+            new_string = new_string + char
+    return new_string
+
+
+def test():
+    """Test data"""
+    my_string = "An aardvark is an animal"
+    new_string = change_string(my_string)
+    print(new_string)       
+    
+
+if __name__ == '__main__':
+    test()
