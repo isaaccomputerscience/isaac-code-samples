aa4f609a50ecedeea66f6ca747af8e2f682589b8
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 10:23:48 +0100

    Variable names

--- subroutines/call-stack/calculate_product.cs
+++ subroutines/call-stack/calculate_product.cs
@@ -11,29 +11,28 @@ To run this file you need to:
 
 using System;
 
-namespace isaac_code_samples
+namespace Subroutines
 {
-    class CallStackExample
+    class CallStack
     {
         // Simple program to demonstrate the use of call stacks
-        static void Main(string[] args){
-            
+        static void Main(string[] args) {            
             Console.WriteLine("Please enter a number");
-            string num1_input = Console.ReadLine();
-            int num1 = Convert.ToInt32(num1_input);
+            string num1String = Console.ReadLine();
+            int num1 = Convert.ToInt32(num1String);
             Console.WriteLine("Please enter another number");
-            string num2_input = Console.ReadLine();
-            int num2 = Convert.ToInt32(num2_input);
+            string num2String = Console.ReadLine();
+            int num2 = Convert.ToInt32(num2String);
             CalculateProduct(num1, num2);
-
         }
+        
 
         // Calculates the product of two numbers
-        static void CalculateProduct(int n1, int n2){
-
+        static void CalculateProduct(int n1, int n2) {
             int product = n1 * n2;
             Console.WriteLine(product);
-
         }
+        
+        
     }
 }
