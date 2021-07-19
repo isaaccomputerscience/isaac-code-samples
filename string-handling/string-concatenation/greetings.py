62e58cbd17ba0eac1abec27f3b570b64a5837cc3
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 10:49:21 +0100

    Applied standards

--- string-handling/string-concatenation/greetings.py
+++ string-handling/string-concatenation/greetings.py
@@ -1,10 +1,13 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
 def greetings():
     """Demonstration of string concatenation"""
     first_name = input("Please enter your name ")
     print("Hello " + first_name)
-    surname = input("Please enter your surname ")
-    full_name = first_name + " " + surname
-    print("Hello " + full_name)
+    last_name = input("Please enter your surname ")
+    user_name = first_name + " " + surname
+    print("Hello " + user_name)
 
 if __name__ == "__main__":
     greetings()
