5e30380e6230ed4b0e1f16f3f8827f60b6a5ebcb
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 11:05:45 +0100

    Create demonstrate-nulls.py

---
+++ string-handling/string-length/demonstrate-nulls.py
@@ -0,0 +1,14 @@
+def demonstrate_nulls():
+    """Demonstrates the difference between a null and a space"""
+    null_string = ""
+    print(len(null_string))
+    string_with_space = " "
+    print(len(string_with_space))
+    if null_string == string_with_space:
+        print("We are the same")
+    else:
+        print("We are different")
+
+
+if __name__ == '__main__':
+    demonstrate_nulls()
