f14d499088f2bed617d4c238df347e77c97c9ca0
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 12:44:24 +0100

    Create show_position.py

---
+++ string-handling/string-position/show_position.py
@@ -0,0 +1,12 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def show_position():
+    """Demonstrates how to find a character within a string"""
+    subject = "Computer Science"
+    position = subject.find("m")
+    print(f"Found at: {position}")
+    
+
+if __name__ == '__main__':
+    show_position()
