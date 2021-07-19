9e31c901666ad4b28dc5c88ab4b69b117e5e37c9
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-19 10:59:44 +0100

    Create message-alert.py

---
+++ string-handling/string-concatenation/message-alert.py
@@ -0,0 +1,10 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def message_alert():
+    """Example of type conversion for string concatenation"""
+    messages = 5
+    print("You have " + str(messages) + " new messages")
+
+if __name__ == "__main__":
+    message_alert()
