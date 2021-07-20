cea4db511cb93b8890d586c7a0b33f4d578ca86c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-20 10:24:05 +0100

    Create length_check.py

---
+++ string-handling/string-validation/length_check.py
@@ -0,0 +1,30 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def check_password(password):
+    """Checks that a password length is at least 8"""
+    is_valid = True
+    if len(password) < 8:
+        is_valid = False
+    return is_valid
+
+
+def test():
+    #normal data
+    password = "elephant1234"
+    result = check_password(password)
+    print(f"{password} is valid: {result}")
+    
+    #boundary data 
+    password = "elephant" # 8 characters
+    result = check_password(password)
+    print(f"{password} is valid: {result}")  
+    
+    #too short
+    password = "1234" # 4 characters
+    result = check_password(password)
+    print(f"{password} is valid: {result}")
+    
+
+if __name__ == '__main__':
+    test()
