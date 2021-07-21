b39c7fc39ae8f2835925ee13fb9ecb3f0607897c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-21 17:00:13 +0100

    Update and rename gcse-programming-concepts/programming-constructs/count_controlled_loop.cs to gcse-programming-concepts/programming-constructs/c-sharp/count_controlled_loop.cs

---
+++ gcse-programming-concepts/programming-constructs/c-sharp/count_controlled_loop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace IsaacCodeSamples
+{
+    
+    class ProgrammingConstructs  // Count controlled loop
+    {
+        
+        // For loop that prints a line five times
+        void Main() {           
+            for (int i = 0; i < 2; i++) {
+                Console.WriteLine("Hip, Hip, Hooray!");
+            }
+        }
+        
+        
+    }
+}
