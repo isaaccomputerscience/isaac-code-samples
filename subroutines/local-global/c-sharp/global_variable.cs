b3c1f5d5fa42df1829ecd0e8d8e986dc0e53a580
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-21 12:40:33 +0100

    Update and rename subroutines/local-global/global_variable.cs to subroutines/local-global/c-sharp/global_variable.cs

--- subroutines/local-global/c-sharp/global_variable.cs
+++ subroutines/local-global/c-sharp/global_variable.cs
@@ -1,45 +1,44 @@
 /*
-Note: This file is designed to be copied out and compiled on your machine. 
-In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
 To run this file you need to:
 1. Copy the contents
 2. Paste them into the C# IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
+3. Change the namespace to match your project (if neccesary)
+4. Compile the program
+5. Run the program
 */
 
 using System;
 
-
-namespace isaac_code_samples
+namespace IsaacCodeSamples
 {
 
     class Global
     {
         public static double vatRate = 0.2;
-
     }
 
-    class Orders
+    class LocalGlobal
     {
 
         // The Main method is the entry point for all C# programs
-        public static void Main(string[] args)
-        {
+        public static void Main() {
             CalculateStandardVat();
         }
+        
 
         // Calculate vat
-        public static void CalculateStandardVat()
-        {
-
+        public static void CalculateStandardVat() {
             Console.Write("Enter amount to calculate VAT on: £");
             string amount = Console.ReadLine();
             double amountNumeric = double.Parse(amount);
             double vat = amountNumeric * Global.vatRate;
             Console.WriteLine($"The amount of vat is {vat}");
-
         }
+        
 
     }
 }
