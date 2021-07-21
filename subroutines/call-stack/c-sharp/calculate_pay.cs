d5afd93ff7d63a36cfd44098e18183c2db988a68
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-21 11:56:06 +0100

    Update and rename subroutines/call-stack/calculate_pay.cs to subroutines/call-stack/c-sharp/calculate_pay.cs

--- subroutines/call-stack/c-sharp/calculate_pay.cs
+++ subroutines/call-stack/c-sharp/calculate_pay.cs
@@ -1,23 +1,25 @@
 /*
-
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
 Note: This file is designed to be copied out and compiled on your machine.
 In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
-
 To run this file you need to:
 1. Copy the contents
 2. Paste them into the C# IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
+3. Change the namespace to match your project (if neccesary)
+4. Compile the program
+5. Run the program
 */
 
 using System;
 
-namespace Subroutines
+namespace IsaacCodeSamples    
 {
+    
     class CallStack
     {
+        
         // Simple program to demonstrate the use of call stacks
-
         static void Main(string[] args) {            
             Console.WriteLine("Enter hours worked ");
             string hoursInput = Console.ReadLine();
@@ -29,12 +31,15 @@ namespace Subroutines
             int pay = CalculatePay(hoursFloat, rateFloat);
             Console.WriteLine(pay);            
         }
+        
 
         // A simple pay calculation program
         static float CalculatePay(float h, float r) {
             float pay = h * r;
             return pay;
         }
+        
+        
     }
 }
 
