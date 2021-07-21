54af09ec59f323c55a509cacdcbaf2977926ea64
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-21 17:16:04 +0100

    Update and rename searching-algorithms/binary-search/binary_search_recursive.cs to searching-algorithms/binary-search/c-sharp/binary_search_recursive.cs

--- searching-algorithms/binary-search/c-sharp/binary_search_recursive.cs
+++ searching-algorithms/binary-search/c-sharp/binary_search_recursive.cs
@@ -1,34 +1,39 @@
 /*
-Note: This file is designed to be copied out and compiled on your machine. 
-In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
-
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
 To run this file you need to:
 1. Copy the contents
 2. Paste them into the C# IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
+3. Change the namespace to match your project (if neccesary)
+4. Compile the program
+5. Run the program
 */
 
 using System;
 
-namespace isaac_code_samples
+namespace IsaacCodeSamples
 {
-    class BinarySearchRecursive
+
+    class BinarySearch  // Recursive version
     {
+    
         // The Main method is the entry point for all C# programs
-        static void Main(string[] args){
+        public static void Main() {
            test(); 
         }
+        
 
         // Performs a binary search recursively
-        public static int BinarySearch(int[] items, int search_item, int first, int last){
-            if (first > last){
+        public static int BinarySearch(int[] items, int search_item, int first, int last) {
+            if (first > last) {
                 return -1;
             } else {
                 int midpoint = (first + last) / 2;
-                if (items[midpoint] == search_item){
+                if (items[midpoint] == search_item) {
                     return midpoint;
-                } else if (search_item > items[midpoint]){
+                } else if (search_item > items[midpoint]) {
                     first = midpoint + 1;
                     return BinarySearch(items, search_item, first, last);
                 } else {
@@ -37,8 +42,10 @@ namespace isaac_code_samples
                 }
             }
         }
+        
 
-        private static void test(){
+        // Test function for binary search
+        private static void test() {
             int[] items = new int[]{5, 12, 24, 56, 68, 72, 81, 95};
 
             //Search for first item in array
@@ -56,5 +63,7 @@ namespace isaac_code_samples
             result = BinarySearch(items, 36, 0, items.Length - 1);
             Console.WriteLine("The search result was: " + result.ToString());
         }
+        
+        
     }
 }
