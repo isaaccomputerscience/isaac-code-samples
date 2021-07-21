47558b12e181b73e8d4e89a46ff46ccded8f0a57
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-21 11:26:12 +0100

    Update and rename string-handling/string-position/file_extension.cs to string-handling/string-position/c-sharp/file_extension.cs
    Applied standards

--- string-handling/string-position/c-sharp/file_extension.cs
+++ string-handling/string-position/c-sharp/file_extension.cs
@@ -1,20 +1,19 @@
 /*
 Isaac Computer Science
 Usage licensed under the Open Government Licence v3.0
-
-Note: This file is designed to be copied out and compiled on your machine. 
-In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file. 
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
 To run this file you need to:
 1. Copy the contents
 2. Paste them into the C# IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
+3. Change the namespace to match your project (if neccesary)
+4. Compile the program
+5. Run the program
 */
 
 using System;
 
-
-namespace StringHandling
+namespace IsaacCodeSamples
 {
 
     class StringPosition
