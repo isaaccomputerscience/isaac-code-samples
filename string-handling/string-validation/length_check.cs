92dbce68468e130152a017981e159f76a75335bb
m-bowley <36036152+m-bowley@users.noreply.github.com>
2021-07-21 11:02:17 +0100

    Adding new point into string at top

--- string-handling/string-validation/length_check.cs
+++ string-handling/string-validation/length_check.cs
@@ -7,8 +7,9 @@ In order for it to compile properly you need to ensure that the project name is 
 To run this file you need to:
 1. Copy the contents
 2. Paste them into the C# IDE of your choice (Visual Studio, for example)
-3. Compile the program
-4. Run the program
+3. Change the namespace to match your project (if neccesary)
+4. Compile the program
+5. Run the program
 */
 
 using System;
