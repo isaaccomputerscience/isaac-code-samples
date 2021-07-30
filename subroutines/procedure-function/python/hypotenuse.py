50e8840e1ae4f3eff63db77ba82752bae5d45092
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-07-30 12:56:00 +0100

    moved import statement

--- subroutines/procedure-function/python/hypotenuse.py
+++ subroutines/procedure-function/python/hypotenuse.py
@@ -2,6 +2,8 @@
 # Usage licensed under the Open Government Licence v3.0
 # https://isaaccomputerscience.org/concepts/prog_sub_proc_fun
 
+import math
+
 
 def calculate_area(length):
     """Calculates the area of a square of a given side length"""
@@ -11,10 +13,8 @@ def calculate_area(length):
 
 def get_hypotenuse(a, b):
     """Calculates the length of the hypotenuse of a right angled triangle"""
-    import math
     hypotenuse = math.sqrt( calculate_area(a) + calculate_area(b) ) 
     return hypotenuse
-
 
 
 def main():
