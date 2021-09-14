d688cee7e386aaeaffb400df6cba80bc210c1074
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2021-09-14 13:55:44 +0100

    Update local_variable.py

--- subroutines/local-global/python/local_variable.py
+++ subroutines/local-global/python/local_variable.py
@@ -2,20 +2,22 @@
 # Usage licensed under the Open Government Licence v3.0
 # https://isaaccomputerscience.org/concepts/prog_sub_local_and_global
 
+def double(n):
+    """Doubles a number and displays result"""
+    amount = n * 2
+    print(amount)
 
-def calculate_reduced_vat():
-    """Calculate vat at a reduced rate"""
+def triple(n):
+    """Triples a number and displays result"""
+    amount = n * 3
+    print(amount)
 
-    # Local variable also called vat_rate overrides the vat_rate from global scope
-    vat_rate = 0.05 
-
-    amount = float(input("Enter amount to calculate VAT on: "))
-    vat = amount * vat_rate
-    print(vat)
-
-
+def main():
+    amount = 100
+    print(amount)
+    double(amount)
+    triple(amount)
+    print(amount)
 
 if __name__ == '__main__':
-    # Global variable vat_rate is defined within the global scope
-    vat_rate = 0.2
-    calculate_reduced_vat()
+    main() 
