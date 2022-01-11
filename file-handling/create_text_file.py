3f25d602bc0f78dd1c0c2f6c5bac90a6ae542bd1
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-01-11 13:47:08 +0000

    Add files via upload

---
+++ file-handling/create_text_file.py
@@ -0,0 +1,14 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+     
+
+def create_playlist():
+    """Simple example of creating a text file"""
+    playlist = open("playlist.txt", mode = "w")
+    playlist.close()  
+    
+
+if __name__ == "__main__":
+    create_playlist()
+
+    
