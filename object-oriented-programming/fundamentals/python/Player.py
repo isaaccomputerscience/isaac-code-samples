3693e375374b048017337be33b6c42cee0c3d2ea
Laura Sach <laura.sach@raspberrypi.org>
2022-01-18 14:50:44 +0000

    Add code for two classes

---
+++ object-oriented-programming/fundamentals/python/Player.py
@@ -0,0 +1,14 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+class Player:
+
+    def __init__(self, given_name):
+        self.__name = given_name
+        self.__score = 0
+    
+
+if __name__ == '__main__':
+
+    # Instantiate an example Player object
+    player1 = Player("Leona")
---
+++ object-oriented-programming/fundamentals/python/PlayingCard.py
@@ -0,0 +1,15 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+class PlayingCard:
+
+    def __init__(self, given_suit, given_rank, given_value):
+        self.__suit = given_suit
+        self.__rank = given_rank
+        self.__value = given_value
+    
+
+if __name__ == '__main__':
+    
+    # Instantiate an example PlayingCard object
+    two_of_clubs = PlayingCard("clubs", "2", 2)
