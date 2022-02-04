51e19f0f61b086adf9f73ca7f69867382d3bd9ed
Ben Garside <ben.garside@raspberrypi.org>
2022-02-04 10:13:49 +0000

    more stuff

--- programming-concepts/input-output/python/age_months.py
+++ programming-concepts/input-output/python/age_months.py
@@ -1,6 +1,19 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
 
-print("How old are you?")
-user_response = input()
-age = int(user_response)
-months = age * 12
-print(f"Wow, you have been alive for around {months} months!")
+
+def main():
+    print("How old are you?")
+    user_response = input()
+    age = int(user_response)
+    months = age * 12
+    print(f"Wow, you have been alive for around {months} months!")
+
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+
+
+if __name__ == '__main__':
+    main()   
+
