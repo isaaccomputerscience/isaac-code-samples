fd343b71aa7504e5dc11c9ad9435aa0310b98003
allenheard <53428550+allenheard@users.noreply.github.com>
2022-02-04 09:58:24 +0000

    a

--- programming-concepts/logical-operators/python/nand.py
+++ programming-concepts/logical-operators/python/nand.py
@@ -1,8 +1,14 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+def main():
+    cold = True
+    wet = False
 
-cold = True
-wet = False
-
-if not(cold and wet):
-    print("It's OK to go outside")
-else:
-    print("I'm going to stay inside")
+    if not(cold and wet):
+        print("It's OK to go outside")
+    else:
+        print("I'm going to stay inside")
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    main()
--- programming-concepts/logical-operators/python/nor.py
+++ programming-concepts/logical-operators/python/nor.py
@@ -1,8 +1,14 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+def main():
+    tired = False
+    early_start = False
 
-tired = False
-early_start = False
-
-if not(tired or early_start):
-    print("I will go out tonight")
-else:
-    print("I'm going to stay at home")
+    if not(tired or early_start):
+        print("I will go out tonight")
+    else:
+        print("I'm going to stay at home")
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    main()
