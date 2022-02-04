bc7e68085b87bd81dd6542214229a8c1106b1206
Ben Garside <ben.garside@raspberrypi.org>
2022-02-04 10:12:58 +0000

    updated stuff

--- programming-concepts/arithmetic-operators/python/exponentiation.py
+++ programming-concepts/arithmetic-operators/python/exponentiation.py
@@ -1,3 +1,16 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
 
-result = 4 ** 3
-print(result)
+
+def main():
+    result = 4 ** 3
+    print(result)
+
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+
+
+if __name__ == '__main__':
+    main()
+    
