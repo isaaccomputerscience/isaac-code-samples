80420bafe7da8e59a949531de6158160f1e042e3
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-02-24 14:12:21 +0000

    Added standard comment

--- object-oriented-programming/fundamentals/python/Deck.py
+++ object-oriented-programming/fundamentals/python/Deck.py
@@ -12,27 +12,27 @@ class Deck:
         suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
         ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
         values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+        
             
         for i in range(4):
             for j in range(13):
                 new_card = PlayingCard(suits[i], ranks[j], values[j])
                 self.__cards.append(new_card)
-            
-       
         
     
     def shuffle(self):
         # Code to shuffle cards here
         pass
+    
 
     def deal(self, num_cards):
         # Code to remove and return cards here
         pass
 
 
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
 
-if __name__ == '__main__':
-    
+if __name__ == '__main__':   
     # Instantiate an example Deck object
-    my_deck = Deck()
- 
+    my_deck = Deck() 
