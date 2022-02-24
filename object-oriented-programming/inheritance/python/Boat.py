ea80ad82cf41fc20da55a511b0a1620456b07c42
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-02-24 14:19:42 +0000

    added main and standard comment

--- object-oriented-programming/inheritance/python/Boat.py
+++ object-oriented-programming/inheritance/python/Boat.py
@@ -12,3 +12,12 @@ class Boat:
         self.__unit_cost = given_unit_cost
 
 
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    name = "Sir David Attenborough"
+    length = 128.9
+    capacity = 88
+    berths = 90
+    cost = 200000000
+    boaty = Boat(name, length, capacity, berths, cost)
