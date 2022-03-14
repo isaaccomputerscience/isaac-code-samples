91e60a885c0d72a10ec918de42e1ba5ecc5c4519
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-03-14 13:33:13 +0000

    Updated to reflect code on platform

--- file-handling/handling-csv-files/python/csv_file_read.py
+++ file-handling/handling-csv-files/python/csv_file_read.py
@@ -1,15 +1,20 @@
 # Isaac Computer Science
 # Usage licensed under the Open Government Licence v3.0
 
-import csv
+import csv 
 
 def read_csv():
-   """Read all the records from a CSV file"""
+   """Read and display the records from a CSV file"""
    filename = "playlist.csv"
-   with open(filename, mode = "r") as playlist:
-       playlist = csv.reader(playlist)
-       for line in playlist:
-           print(line)
+   with open(filename, mode="r") as playlist:
+       csv_reader = csv.reader(playlist, delimiter=",")
+       count = 0
+       for line in csv_reader:
+          if count == 0:  # Process header record
+              print(f"Header: {line[0]}, {line[1]}, {line[2]}")
+          else:  # Process data record
+              print(f"Data: {line[0]}, {line[1]}, {line[2]}")
+          count = count + 1
 
 
 # This code will run if this file is executed directly
