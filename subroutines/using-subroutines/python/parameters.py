f9f663e06a225d426a40675a10cbe6c725484665
Matt Hogan <matt.hogan@raspberrypi.org>
2022-04-05 16:54:17 +0100

    added GCSE parameters python code

---
+++ subroutines/using-subroutines/python/parameters.py
@@ -0,0 +1,16 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_sub_proc_fun
+
+def calculate_two_numbers(number1, number2):
+    """Adds two numbers and displays result"""
+    return (answer)
+
+def main():
+    number1 = int(input("Enter a number"))
+    number2 = int(input("Enter another number"))
+    answer = number1 + number2
+    print(answer)
+
+if __name__ == '__main__':
+    main()
