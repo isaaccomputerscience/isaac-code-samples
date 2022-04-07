2edcf086b46da64e5aad121fc4648cea627d0e0d
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-04-07 16:16:21 +0100

    added comment and call to main

--- subroutines/define-and-call/python/define_call.py
+++ subroutines/define-and-call/python/define_call.py
@@ -5,6 +5,10 @@
 def greet_me():
     """Print a basic greeting"""
     print("Hello there!")
-
+    
+def main():
+    greet_me()    
+    
+# This code will run if this file is executed directly (i.e. not called by another program)
 if __name__ == '__main__':
-    greet_me()
+    main()
