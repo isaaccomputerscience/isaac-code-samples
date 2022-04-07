fa11081d2a79fc022b725ded2dd6da75546a7b90
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-04-07 16:26:46 +0100

    updated to match platform

--- subroutines/define-and-call/python/run_quiz.py
+++ subroutines/define-and-call/python/run_quiz.py
@@ -3,16 +3,19 @@
 # https://isaaccomputerscience.org/concepts/prog_sub_define_and_call
 
 def run_quiz():
-    """Ask a quiz question"""
-    print("What is the capital city of Botswana?")
-    answer = input()
-
+    """A very simple quiz"""
+    answer = input("What is the capital city of Botswana? ")
+    if answer == "Gaborone":
+        print("Well done!")
+    else:
+        print("Sorry! That is the wrong answer")
 
 def main():
     name = input("What is your name? ")
+    print(f"Welcome {name}")
     run_quiz() 
-    print("End of the quiz")
-
-
+    print("End of the quiz")   
+    
+# This code will run if this file is executed directly (i.e. not called by another program)
 if __name__ == '__main__':
     main()
