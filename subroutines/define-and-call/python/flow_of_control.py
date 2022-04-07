2650329ca746f7a7f83f33f9dffdb4369b61bb45
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-04-07 16:21:09 +0100

    added comment for main

--- subroutines/define-and-call/python/flow_of_control.py
+++ subroutines/define-and-call/python/flow_of_control.py
@@ -10,5 +10,6 @@ def main():
     print(f"Seconds: {seconds}, Milliseconds: {milliseconds}")
 
 
+# This code will run if this file is executed directly (i.e. not called by another program)
 if __name__ == '__main__':
     main()
