ff42bda7d0927626807506874fcc24cb22b99518
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-04-11 09:58:27 +0100

    changed function call to match c#

--- subroutines/using-subroutines/python/parameters.py
+++ subroutines/using-subroutines/python/parameters.py
@@ -3,13 +3,11 @@
 # https://isaaccomputerscience.org/concepts/prog_sub_proc_fun
 
 def calculate_two_numbers(number1, number2):
-    """Adds two numbers and displays result"""
-    return (answer)
+    answer = number1 + number2
+    return answer
 
 def main():
-    number1 = int(input("Enter a number"))
-    number2 = int(input("Enter another number"))
-    answer = number1 + number2
+    answer = calculate_two_numbers(5, 10)
     print(answer)
     
     
