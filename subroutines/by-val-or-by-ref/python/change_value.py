56a4d3679c300d103ed609142e1b4184eefc957d
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-04-25 15:44:26 +0100

    Create change_value.py

---
+++ subroutines/by-val-or-by-ref/python/change_value.py
@@ -0,0 +1,30 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_sub_value_reference
+
+def change_value(item):
+    """demonstrates passing by reference"""
+    print("\n")
+    print(f"Original value in change_value: {item}")
+    object_id = id(item)
+    print(f"Original object id in change_value: {object_id}")
+    item = "cat"
+    print(f"New value in change_value: {item}")
+    object_id = id(item)
+    print(f"New object id in change_value: {object_id}")
+
+def main():
+    word = "dog"
+    print(f"Original value in main: {word}")
+    object_id = id(word)
+    print(f"Original object id in main: {object_id}")
+    change_value(word) # Call function to modify value
+    print("\n")
+    object_id = id(word)
+    print(f"Value in main after function call: {word}")
+    object_id = id(word)
+    print(f"Object id in main after function call: {object_id}")
+
+
+if __name__ == '__main__':
+    main()
