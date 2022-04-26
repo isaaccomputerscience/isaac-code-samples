497da5d3ca1298c2f97bdf6ad631bbf5894915e0
Cris Thoseby <72384308+cristhosebyrasp@users.noreply.github.com>
2022-04-26 17:31:41 +0100

    Create convert_case_upper.py

---
+++ string-handling/string-characteristics/python/convert_case_upper.py
@@ -0,0 +1,17 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def convert_string_case(my_string):
+  my_string = ""
+  converted_string = my_string.upper()
+  return converted_string
+
+def test():
+  my_string = input(str("Full name: "))
+  converted_string = my_string.upper()
+  print(converted_string)
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+  test()
