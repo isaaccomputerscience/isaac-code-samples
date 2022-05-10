ca2c184818c50e0b1275c6b339eb766ce65e4977
Alex Parry <alex.parry@raspberrypi.org>
2022-05-10 15:14:45 +0100

    Creating a dictionary in Python and C#

---
+++ data-structures/dictionaries/python/create_dictionary.py
@@ -0,0 +1,14 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+
+def main():
+    """Create a dictionary and output a value using a key"""
+    results = {"Detra": 17, "Nova": 84, "Charlie": 22}
+    print(results["Detra"])
+
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    main()
