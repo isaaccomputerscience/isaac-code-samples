e3ad51caf6b583234703d3609ccef29e6fdbbaff
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-10 10:50:45 +0100

    Add files via upload

---
+++ object-oriented-programming/game.py
@@ -0,0 +1,65 @@
+from pet_class import Pet
+
+def change_pet(my_pet):
+    """Allows pet to be changed"""
+    answer = input("\nDo you want to change the name of your pet(y/n)? ")
+    if answer.lower() == "y":         
+        new_name = input("Enter a new name for your pet ")
+        my_pet.set_name(new_name)
+    answer = input("\nDo you want to change the type of pet you have(y/n)? ")
+    if answer.lower() == "y":
+        pet_name = my_pet.get_name()
+        new_type = input(f"What type of animal is {pet_name}? ")
+        my_pet.set_type(new_type)
+    answer = input("\nDo you want to change the colour of your pet(y/n)? ")
+    if answer.lower() == "y":
+        pet_name = my_pet.get_name()
+        new_colour = input(f"What colour is {pet_name}? ")
+        my_pet.set_colour(new_colour)
+    print("\n")
+    my_pet.describe()
+
+def show_game_options():
+    """Options for game"""
+    print ("\n")
+    print ("  1. Feed your pet")
+    print ("  2. Play with your pet")
+    print ("  3. Put your pet to bed")
+    print ("  4. Wake your pet up")
+    print ("  5. Change pet")
+    print ("  9. Finish the game")
+    
+def main():
+    pet_name = input("Enter a name for your pet ")
+    pet_type = input(f"What type of animal is {pet_name}? ")
+    pet_colour = input(f"What colour is {pet_name}? ")
+    my_pet = Pet(pet_name, pet_type, pet_colour) # Makes a pet object
+    finished = False
+    while finished == False:
+        show_game_options()
+        choice = input(" What do you want to do? ")
+        print("\n")
+        if choice == "1":
+            my_pet.feed()
+        elif choice == "2":
+            my_pet.play()
+        elif choice == "3":
+            my_pet.sleep()
+        elif choice == "4":
+            my_pet.wake()
+        elif choice == "5":
+            change_pet(my_pet)
+        elif choice == "9":
+            finished = True # This will cause game to end
+            print ("Thanks for playing the pet game")
+        else:
+            print("That is not a menu option")
+
+        
+            
+              
+
+    
+
+if __name__ == "__main__":
+    main()
---
+++ object-oriented-programming/pet_class.py
@@ -0,0 +1,80 @@
+import random
+
+class Pet:
+    """Defines a pet in a children's game"""
+    def __init__(self, given_name, given_type, given_colour):
+        self.name = given_name
+        self.type = given_type
+        self.colour = given_colour
+        all_moods = ["playful", "hungry", "sleepy"]
+        self.mood = random.choice(all_moods)
+        self.sleeping = False
+
+    def get_name(self):
+        return self.name
+
+    def get_type(self):
+        return self.type
+
+    def get_colour(self):
+        return self.colour
+
+    def get_mood(self):
+        return self.mood
+
+    def is_sleeping(self):
+        return self.sleeping
+
+    def describe(self):
+        print(f"I am a {self.mood}, {self.colour} {self.type} called {self.name}")
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+    def set_type(self, new_type):
+        self.type = new_type
+
+    def set_colour(self, new_colour):
+        self.colour = new_colour
+
+    def play(self):
+       if self.sleeping == True:
+           print("Zzzzzzz. I am sleeping")
+       elif self.mood == "hungry":
+           print("I am too hungry to play")
+       elif self.mood == "tired":
+           print("I am too tired to play")
+       else:
+           print("This is fun, I love playing")
+           all_moods = ["playful", "hungry", "sleepy"]
+           self.mood = random.choice(all_moods)
+
+    def feed(self):
+        if self.sleeping == True:
+           print("Zzzzzzz. I am sleeping")
+        if self.mood == "tired":
+            print("I am too sleepy to eat anything now")
+        elif self.mood == "playful":
+            print("I am not hungry - I want to play")
+        else:
+            print("Yum - yum - that tastes great")
+            self.mood = "playful"
+
+    def sleep(self):
+        if self.mood == "playful":
+            print("I am too playful to sleep")
+        elif self.mood == "hungry":
+            print("I need something to eat before I can go to sleep")
+        else:
+            print("Zzzzzzzz")
+            self.sleeping = True
+
+    def wake(self):
+        if self.sleeping == False:
+           print("I wasn't even asleep!")
+        else:
+            print("I am awake and ready to play!")
+            self.sleeping = False
+            self.mood = "playful"
+
+
