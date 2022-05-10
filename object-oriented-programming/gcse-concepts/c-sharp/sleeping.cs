2fa5ac8c74afd1d1d85f5b516864ea896c74f3da
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-10 16:42:38 +0100

    Create sleeping.cs

---
+++ object-oriented-programming/gcse-concepts/c-sharp/sleeping.cs
@@ -0,0 +1,41 @@
+/*
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Change the namespace to match your project (if necessary)
+4. Compile the program
+5. Run the program
+*/
+
+
+using System;
+namespace IsaacCodeSamples
+{
+    class Program
+    {
+        // The Main method is the entry point for all C# programs
+        public static void Main()
+        {
+            Console.WriteLine("Enter a name for your pet ");
+            string petName = Console.ReadLine();
+            Console.WriteLine($"What type of animal is {petName}? ");
+            string petType = Console.ReadLine();
+            Console.WriteLine($"What colour is {petName}? ");
+            string petColour = Console.ReadLine();
+            var myPet = new Pet(petName, petType, petColour); // Makes a pet object
+
+            string name = myPet.GetName();
+            if (myPet.IsSleeping()== true) {
+                Console.WriteLine($"Shhhhhh, {name} is asleep");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is awake");
+            }
+        }
+    }
+ }
