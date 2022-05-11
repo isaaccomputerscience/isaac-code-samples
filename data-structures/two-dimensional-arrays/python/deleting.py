15acdaf3d5dc20480f6cba12e253cd0275776a07
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-11 12:19:29 +0100

    Create deleting.py

---
+++ data-structures/two-dimensional-arrays/python/deleting.py
@@ -0,0 +1,17 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+def main():
+    spelling_words = [
+        ["school", "pull", "where"],
+        ["path", "floor", "sugar"],
+        ["accident", "eight", ""]]
+  
+    spelling_words[0][0] = "" 
+    print(spelling_words)
+
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    main()
