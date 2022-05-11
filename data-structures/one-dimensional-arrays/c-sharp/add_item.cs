1d4917d151a103907a1783bf1956e49b4b72324c
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-11 10:53:54 +0100

    Create add_item.cs

---
+++ data-structures/one-dimensional-arrays/c-sharp/add_item.cs
@@ -0,0 +1,39 @@
+/*
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Change the namespace to match your project (if necessary)
+4. Compile the program
+5. Run the program
+*/
+
+using System;
+
+namespace IsaacCodeSamples
+{
+
+    class OneDimensionalArrays
+    {
+        // The Main method is the entry point for all C# programs
+        public static void Main()
+        {
+            string[] emotions = new string[6]; // Declare an array of 6 elements
+
+            emotions[0] = "amazed";  // Assign a value into a position of the array
+            emotions[1] = "delighted";
+            emotions[2] = "ecstatic";
+            emotions[3] = "enthusiastic";
+            emotions[4] = "lively";
+
+            Console.WriteLine(emotions[2]);
+            
+            emotions[5] = "happy";
+        }
+
+
+    }
+}
