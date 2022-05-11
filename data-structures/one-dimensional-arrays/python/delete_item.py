124f76ca258c3cfaa29aedaaa416e62eac95af51
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-11 11:04:05 +0100

    Create delete_item.py

---
+++ data-structures/one-dimensional-arrays/python/delete_item.py
@@ -0,0 +1,19 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+
+def main():
+    """Create a list and output a value using an index value"""
+    emotions = ["amazed", "delighted", "ecstatic", "enthusiastic", "lively"]
+
+    print(emotions[2])  # Displays the third item in the list
+    
+    emotions.append("happy")  # Appends an item to the end of the list
+    
+    emotions.pop(1)  # Removes the second item from the list
+
+
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
+if __name__ == '__main__':
+    main()
