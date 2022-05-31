9c80414ea8fa6d8ca9e03aa599450a349fafbd3f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-05-31 13:14:47 +0100

    Create app2.py

---
+++ event-driven-programming/tkinter/app2.py
@@ -0,0 +1,30 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+
+from tkinter import *
+
+
+def display_login_form():
+    """Login form for Kiara's cakes"""
+    # Application window
+    login_form = Tk()
+    login_form.title("Kiara's Cakes")
+    login_form.geometry("400x300") # Width x Height
+    login_form.configure(bg="#f5fffa")
+    login_form.option_add("*font", "Verdana 10")
+    
+    # Widgets
+    lbl_userid = Label(login_form)
+    lbl_userid.config(bg="#f5fffa", text="User id")
+    lbl_userid.pack()
+  
+    lbl_password = Label(login_form)
+    lbl_password.config(bg="#f5fffa", text="Password")
+    lbl_password.pack()
+
+    # Mainloop
+    login_form.mainloop()    
+    
+
+if __name__ == "__main__":
+    display_login_form()
