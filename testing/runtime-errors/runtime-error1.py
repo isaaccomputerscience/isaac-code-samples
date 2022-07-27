4b0d3c20c4b72cba7de0b528c124df0f19da0cba
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2022-07-27 11:41:23 +0100

    runtime errors add

---
+++ testing/runtime-errors/runtime-error1.py
@@ -0,0 +1,7 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_softeng_debug
+
+number = input("Please enter a number: ")
+number = int(number)
+print(number)
---
+++ testing/runtime-errors/runtime-error2.py
@@ -0,0 +1,8 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_softeng_debug
+
+with open("highscores.txt", "r") as file_object:
+    lines = file_object.readlines()
+    print(lines)
+    
