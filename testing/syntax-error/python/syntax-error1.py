2bbc8da307e50e5755c50654b1bd68d2183ccb1f
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2022-07-27 11:29:45 +0100

    syntax errors python add

---
+++ testing/syntax-error/python/syntax-error1.py
@@ -0,0 +1,6 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_softeng_debug
+
+users_name = input("Please enter your name: "
+print (users_name)
---
+++ testing/syntax-error/python/syntax_error2.py
@@ -0,0 +1,9 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_softeng_debug
+
+name = input("Please enter your name: ")
+if name == "Ada"
+    print("Hi Ada, you are one of my heroes!")
+else:
+    print("Hello ", name)
