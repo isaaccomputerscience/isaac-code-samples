46b232e202a35cb13eb8ac998ec3b32cbeb81b7a
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2022-07-28 08:37:27 +0100

    password checker python add

---
+++ testing/test-data/password-checker.py
@@ -0,0 +1,10 @@
+# Isaac Computer Science
+# Usage licensed under the Open Government Licence v3.0
+# https://isaaccomputerscience.org/concepts/prog_softeng_test_plans 
+
+def check_password(password):
+    """Checks that a password length is between 8 and 1 characters. Returns True if valid"""
+    is_valid = True
+    if len(password) < 8 or len(password) > 14:
+        is_valid = False
+    return is_valid
