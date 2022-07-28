6222ff7f065737b75e70aa0d9d2a5fe03f9045e6
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2022-07-28 11:02:17 +0100

    runtime error 1 c add

---
+++ testing/runtime-errors/runtime-error1.cs
@@ -0,0 +1,28 @@
+/*
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the
+"namespace" in this file.To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Change the namespace to match your project (if necessary)
+4. Compile the program
+5. Run the program
+*/
+
+using System;
+
+namespace IsaacCodeSamples
+{
+    class Testing
+    {
+        // The Main method is the entry point for all C# programs
+        public static void Main() {
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+        }
+    }
+}
