c6122d3104d9d6314c197768f4ecb41e86aee05a
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-08-15 11:00:55 +0100

    added comments

--- network-security/sql_injection.py
+++ network-security/sql_injection.py
@@ -7,6 +7,7 @@
 import sqlite3
 
 def attempt_login(userid, pword):
+    """Demonstrates a simple SQL injection"""
     try: 
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
@@ -19,21 +20,24 @@ def attempt_login(userid, pword):
             print("Access denied")
     except:
         print ("Failed to connect to database")
+        
 
 def normal_login():
+    """Can be used to emulate a "normal" login"""
     userid = input("Enter your userid: ")  
     pword = input("Enter your password: ") 
     attempt_login(userid, pword)
-
-def hack_that_db():
-    userid = "x"
-    pword = "x' OR 1=1 LIMIT 1 --"
-    attempt_login(userid, pword)
-
     
 
+def hack_that_db():
+    """Can be used to emulate a login using an SQL injection"""
+    userid = "x"
+    pword = "x' OR 1=1 LIMIT 1 --"
+    attempt_login(userid, pword)    
 
 
+# This code will run if this file is executed directly
+# (i.e. not called by another program)
 if __name__ == "__main__":
     normal_login()
-    #hack_that_db()
+    hack_that_db()
