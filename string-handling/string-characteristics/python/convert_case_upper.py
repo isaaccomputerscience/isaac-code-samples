e19a4ee455fc324d3384ac4a078e8dee0b5048e3
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-08-16 14:09:49 +0100

    updated to match platform content

--- string-handling/string-characteristics/python/convert_case_upper.py
+++ string-handling/string-characteristics/python/convert_case_upper.py
@@ -1,20 +1,13 @@
 # Isaac Computer Science
 # Usage licensed under the Open Government Licence v3.0
 
-def convert_to_upper(my_string):
-    ### Demonstrates how to convert a string to upper case ###
-    upper_case_string = my_string.upper()
-    return upper_case_string
-
-
-def test():
-    ### Test data ###
-    name = input("Enter full name: ")
-    converted_string = convert_to_upper(name)
-    print(converted_string)
-    
+def convert_to_upper():
+    """Demonstrates how to convert a string to upper case"""
+    user_name = input("Enter your full name: ")
+    upper_case_name = user_name.upper()
+    print(upper_case_name)
 
 # This code will run if this file is executed directly
 # (i.e. not called by another program)
 if __name__ == '__main__':
-    test()
+    convert_to_upper()
