47419d44f2188aac0052a9e7a95ec59ba0e27637
Laura Holborow <98157398+lauraholborow@users.noreply.github.com>
2022-08-19 16:25:12 +0100

    Update password-checker.cs

--- testing/test-data/password-checker.cs
+++ testing/test-data/password-checker.cs
@@ -6,7 +6,7 @@ Note: This file is designed to be copied out and compiled on your machine.
 In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
 To run this file you need to:
 1. Copy the contents
-2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+2. Paste them into the C# IDE of your choice (Microsoft Visual Studio, for example)
 3. Change the namespace to match your project (if necessary)
 4. Compile the program
 5. Run the program
