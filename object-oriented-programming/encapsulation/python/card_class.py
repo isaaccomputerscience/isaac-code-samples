14ee28c6b3e21b1664aa390daf826cba22c4b8a7
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-09-07 16:48:42 +0100

    Update and rename playing_card_encapsulation.py to card_class.py

--- object-oriented-programming/encapsulation/python/card_class.py
+++ object-oriented-programming/encapsulation/python/card_class.py
@@ -17,16 +17,6 @@ class PlayingCard:
     def get_value(self):
         return self.__value
 
-    """
-    Alternative version using decorators
-
-    def suit(self):
-        return self.__suit
-    
-    @suit.setter
-    def suit(self, new_suit):
-        self.__suit = new_suit    
-    """
 
 # This code will run if this file is executed directly
 # (i.e. not called by another program)
