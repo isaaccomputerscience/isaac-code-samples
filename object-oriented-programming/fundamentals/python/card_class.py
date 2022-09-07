1c9285a8f604047dec4b6b0fed403b07b326f299
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-09-07 11:28:42 +0100

    Update and rename PlayingCard.py to card_class.py

--- object-oriented-programming/fundamentals/python/card_class.py
+++ object-oriented-programming/fundamentals/python/card_class.py
@@ -1,7 +1,7 @@
 # Isaac Computer Science
 # Usage licensed under the Open Government Licence v3.0
 
-class PlayingCard:
+class Card:
 
     def __init__(self, given_suit, given_rank, given_value):
         self.__suit = given_suit
@@ -12,5 +12,5 @@ class PlayingCard:
 # This code will run if this file is executed directly
 # (i.e. not called by another program)
 if __name__ == '__main__':    
-    # Instantiate an example PlayingCard object
-    two_of_clubs = PlayingCard("clubs", "2", 2)
+    # Instantiate an example Card object
+    two_of_clubs = Card("clubs", "2", 2)
