f06952288009ad85becc3fedfeb812bff6b76968
Alex Parry <51757193+alexparry@users.noreply.github.com>
2022-09-08 11:21:39 +0100

    Create sum_to.cs

---
+++ data-structures/complexity/measuring-complexity/c-sharp/sum_to.cs
@@ -0,0 +1,44 @@
+/*
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Change the namespace to match your project (if necessary)
+4. Compile the program
+5. Run the program
+*/
+
+using System;
+
+namespace IsaacCodeSamples
+{
+    class Complexity
+    {
+        // The Main method is the entry point for all C# programs
+        public static void Main()
+        {   
+            // Test the SumTo method
+            int num = 5;
+            int result = SumTo(num);
+            Console.WriteLine($"The sum of all integers from 1 to {num} is {result}");
+        }
+
+
+        // Calculates the sum of all integers from 1 to the given number
+        public static int SumTo(int givenNumber) 
+        {
+            int total = 0;
+            for (int i = 1; i <= givenNumber; i++) {
+                total = total + i;
+            }
+
+            return total;
+        }
+
+        
+    }
+}
