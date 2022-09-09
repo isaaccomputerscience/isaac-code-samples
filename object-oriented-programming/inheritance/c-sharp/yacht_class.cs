68069b3850750d6378ee9501c1adc4900b990298
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-09-09 12:28:56 +0100

    Create yacht_class.cs

---
+++ object-oriented-programming/inheritance/c-sharp/yacht_class.cs
@@ -0,0 +1,58 @@
+/*
+Isaac Computer Science
+Usage licensed under the Open Government Licence v3.0
+
+Note: This file is designed to be copied out and compiled on your machine.
+In order for it to compile properly you need to ensure that the project name is the same as the "namespace" in this file.
+To run this file you need to:
+1. Copy the contents
+2. Paste them into the C# IDE of your choice (Visual Studio, for example)
+3. Change the namespace to match your project (if necessary)
+4. Compile the program
+5. Run the program
+*/
+
+using System;
+using System.IO;
+
+
+namespace IsaacCodeSamples
+{
+
+
+    class Yacht : Boat  // Inherits from Boat
+    {
+
+        private string name;
+        private float length;
+        private int capacity;
+        private int berths;
+        protected float unitCost;
+
+        // Constructor method
+        public Yacht(base : string givenName, float givenLength, int givenCapacity, int givenBerths, float givenUnitCost) {
+            name = givenName;
+            length = givenLength;
+            capacity = givenCapacity;
+            berths = givenBerths;
+            unitCost = givenUnitCost;
+        }
+        
+    }
+
+
+    class Testing
+    {
+        // The Main method is the entry point for all C# programs
+        public static void Main() {
+            string name = "Sir David Attenborough";
+            float length = 128.9F;
+            int capacity = 88;
+            int berths = 90;
+            float cost = 200000000F;
+            Boat boaty = new Boat(name, length, capacity, berths, cost);  // Instantiate a new boat object
+        }
+        
+    }
+    
+}
