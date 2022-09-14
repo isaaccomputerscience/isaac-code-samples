0b06e9c0fff82ef081e7a8d7b53dfb2e0eab3e1f
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-09-14 14:00:47 +0100

    Update card_class.py

--- object-oriented-programming/blackjack-game/python/card_class.py
+++ object-oriented-programming/blackjack-game/python/card_class.py
@@ -2,7 +2,7 @@
 # Usage licensed under the Open Government Licence v3.0
 # BETA VERSION
 
-class Card:
+class PlayingCard:
     """Defines a standard playing card"""
 
     def __init__(self, given_suit, given_rank, given_value):
@@ -10,17 +10,20 @@ class Card:
         self.__rank = given_rank
         self.__value = given_value
 
-
     def get_suit(self):
+        """Returns the suit of the card"""
         return self.__suit
 
     def get_rank(self):
+        """Returns the rank of the card"""
         return self.__rank
 
     def get_value(self):
+        """Returns the value of the card"""
         return self.__value
 
-    def __repr__(self):
+    def get_description(self):
+        """Returns suit and rank as a string"""
         return(f"{self.__rank} of {self.__suit}")
     
 
@@ -28,4 +31,6 @@ class Card:
 # (i.e. not called by another program)
 
 if __name__ == '__main__':        
-    two_of_clubs = Card("clubs", "2", 2) # Instantiate an example Card object
+    my_card = Card("clubs", "2", 2) # Instantiate an example Card object
+    description = my_card.get_description()
+    print(description)
