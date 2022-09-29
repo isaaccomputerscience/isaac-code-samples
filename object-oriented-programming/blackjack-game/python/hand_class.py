b41a565f97472464902ae055d695f346822a6923
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-09-29 13:47:31 +0100

    Update hand_class.py

--- object-oriented-programming/blackjack-game/python/hand_class.py
+++ object-oriented-programming/blackjack-game/python/hand_class.py
@@ -7,8 +7,8 @@ class Hand:
     def __init__(self):
         """Constructor"""
         self.__cards = []
-        self.__value = 0
-        self.__aces = 0
+        self.__value = 0  # value of hand
+        self.__aces = 0  # number of aces in hand
 
     def add_card(self, card):
         """Adds a card to the hand and calculates the new value of the hand"""
@@ -16,11 +16,11 @@ class Hand:
         if card.get_rank() == "Ace":
             self.__aces += 1
         self.__value += card.get_value()
-        if self.__value > 21: # adjust for aces
-            aces = self.__aces
-            while self.__value > 21 and aces > 0:
-                self.__value -= 10
-                self.__aces -= 1
+        if self.__value > 21:  # if the value of hand is over 21
+            aces = self.__aces  # get number of aces in hand
+            while self.__value > 21 and aces > 0:  # if there are aces left to evaluate and value of hand is still over 21
+                self.__value -= 10  # take the value of an ace as 1
+                aces -= 1  # adjust number of aces to evaluate
 
     def get_cards(self):
         """Returns the cards in the hand"""
