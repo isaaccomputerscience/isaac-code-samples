ab6f6ef3343e4b9053f78e88222b294d003739f3
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-10-04 17:06:00 +0100

    tweaked to match platform

--- object-oriented-programming/encapsulation/python/tkinter_example.py
+++ object-oriented-programming/encapsulation/python/tkinter_example.py
@@ -4,16 +4,25 @@
 from tkinter import *
 
 class tkinter_example():
+    
     def __init__(self, master):
+
+        self.lbl_prompt = Label(master, text = "Enter your name") # Creates instance of label wigdet
+        self.lbl_prompt.pack() # Positions entry widget on the form
+        
         self.txt_user_name = Entry(master) # Creates instance of entry widget
         self.txt_user_name.pack() # Positions entry widget on the form
+        
+        self.btn_play = Button(master, text="Play Game!", command=self.welcome) # Creates instance of button widget
+        self.btn_play.pack() # Positions button on the form
 
-        self.btn_push_me = Button(master, text="Push me!", command=self.display_name) # Creates instance of button widget
-        self.btn_push_me.pack() # Positions button on the form
+        self.lbl_user_name = Label(master) # Creates instance of label wigdet
+        self.lbl_user_name.pack() # Positions entry widget on the form
 
-    def display_name(self):
+    def welcome(self):
         user_name = self.txt_user_name.get() # Gets the value from the text entry box
-        print(user_name) # will display the name entered in the console window (just for testing)
+        msg = "Welcome to Blackjack " + user_name
+        self.lbl_user_name.config(text=msg)
     
 
 # This code will run if this file is executed directly
@@ -21,6 +30,7 @@ class tkinter_example():
 
 if __name__ == '__main__':
     master = Tk() # Create an instance of a tkinter window
+    master.title("BlackJack")
     master.geometry("500x500") # Make the window big enough to see
     gui = tkinter_example(master)
     master.mainloop() # Run the application
