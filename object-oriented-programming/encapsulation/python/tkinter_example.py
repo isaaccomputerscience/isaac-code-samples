4dd71ea8d5f32e81f02e9a932c507cfe2638ba74
Diane Dowling <5488434+dianedowling@users.noreply.github.com>
2022-10-04 16:31:42 +0100

    Update tkinter_example.py

--- object-oriented-programming/encapsulation/python/tkinter_example.py
+++ object-oriented-programming/encapsulation/python/tkinter_example.py
@@ -1,22 +1,26 @@
 # Isaac Computer Science
 # Usage licensed under the Open Government Licence v3.0
 
-from tkinter import Tk, StringVar, Entry
+from tkinter import *
 
-def tkinter_example():
-    master = Tk()
-    master.geometry("500x500")  # Make the window big enough to see
+class tkinter_example():
+    def __init__(self, master):
+        self.txt_user_name = Entry(master) # Creates instance of entry widget
+        self.txt_user_name.pack() # Positions entry widget on the form
 
-    var_user_name = StringVar() # Creates instance of tkinter variable
-    txt_user_name = Entry(master, textvariable=var_user_name) # Creates instance of entry widget
-    txt_user_name.pack() # Positions entry widget on the form
-    user_name = var_user_name.get() # Gets the value from the tkinter variable
+        self.btn_push_me = Button(master, text="Push me!", command=self.display_name) # Creates instance of button widget
+        self.btn_push_me.pack() # Positions button on the form
 
-    master.mainloop()
-
+    def display_name(self):
+        user_name = self.txt_user_name.get() # Gets the value from the text entry box
+        print(user_name) # will display the name entered in the console window (just for testing)
+    
 
 # This code will run if this file is executed directly
 # (i.e. not called by another program)
 
 if __name__ == '__main__':
-    tkinter_example()
+    master = Tk() # Create an instance of a tkinter window
+    master.geometry("500x500") # Make the window big enough to see
+    gui = tkinter_example(master)
+    master.mainloop() # Run the application
